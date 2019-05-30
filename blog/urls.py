d524bb0e55f74f4fdaf514fea97458bad9a5d978
Gustavo <gustavosilvaoliver555@gmail.com>
2019-05-29 21:20:03 -0300

    fertg

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list, name='post_list'),
+    path('projeto', views.projeto, name='Projetos'),
+    path('post/<int:pk>/',views.post_detail, name = 'post_detail'),
 ]
+
+
+
